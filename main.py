a4c57becf31688380bbf690094f5387ec2b01a46
seoilgun <seoilgun@gmail.com>
2020-08-01 13:53:36 +0900

    update

--- main.py
+++ main.py
@@ -473,7 +473,7 @@ def demo1(image_paths, target_layer, arch, topk, model_path, input_size, num_cla
                     grad_cam_im = Image.open(grad_cam_path)
                     guided_grad_cam_im = Image.open(guided_grad_cam_path)
                     grad_cam_bbox_im = Image.open(grad_cam_bbox_path)
-
+                    print(grad_cam_im.size, guided_grad_cam_im.size, grad_cam_bbox_im.size)
                     im_h = cv2.hconcat([raw_images[j], np.array(grad_cam_im), np.array(guided_grad_cam_im), np.array(grad_cam_bbox_im)])
                     concat_im = Image.fromarray(im_h)
                     concat_w, concat_h = concat_im.size
